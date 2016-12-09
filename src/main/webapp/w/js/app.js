0626949fdd18840bbef4d7c79a832d38aa0f6b2e
xdlv <kkjattuu@163.com>
2016-12-09 17:23:37 +0800

    mdgb menu

--- src/main/webapp/w/js/app.js
+++ src/main/webapp/w/js/app.js
@@ -1,4 +1,49 @@
 var app = angular.module('taxApp', ['ngMaterial']);
-app.controller('taxController', ['$scope', function($scope){
-	
+app.controller('taxController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+    $scope.menus = [{
+        text: '用户管理',
+        children: [{
+            text: '新建用户'
+        }, {
+            text: '删除用户'
+        }]
+    }, {
+        text: '项目管理',
+        children: [{
+            text: '新建项目'
+        }, {
+            text: '删除项目'
+        }]
+    }];
+
+    $scope.toggleMenu = function() {
+        $mdSidenav('left').toggle();
+    };
+    $scope.toggleItem = function(index) {
+        alert(index);
+    };
+    $scope.isOpen = function(section) {
+          return true;
+    };
+    
+    $scope.sections = [{
+        name: 'Beers',
+        type: 'toggle',
+        pages: [{
+            name: 'IPAs',
+            type: 'link',
+            state: 'beers.ipas',
+            icon: 'fa fa-group'
+        }, {
+            name: 'Porters',
+            state: 'home.toollist',
+            type: 'link',
+            icon: 'fa fa-map-marker'
+        }, {
+            name: 'Wheat',
+            state: 'home.createTool',
+            type: 'link',
+            icon: 'fa fa-plus'
+        }]
+    }];
 }]);
